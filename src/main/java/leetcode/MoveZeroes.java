0755957868265e7106a29b4da38ce3076a6a8b56
UmaShankarN <numashankar.psg@gmail.com>
2022-10-26 15:50:21 +0100

    check

---
+++ src/main/java/leetcode/MoveZeroes.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class MoveZeroes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//283. Move Zeroes
+		//https://leetcode.com/problems/move-zeroes/
+		
+		/*
+		 * 
+		 */
+		
+		
+	}
+
+}
