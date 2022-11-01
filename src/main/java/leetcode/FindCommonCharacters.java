b5d5fe4d5cb7ea27cbd14533df1085243a5058dc
UmaShankarN <numashankar.psg@gmail.com>
2022-11-01 16:31:38 +0000

    working code

--- src/main/java/leetcode/FindCommonCharacters.java
+++ src/main/java/leetcode/FindCommonCharacters.java
@@ -9,47 +9,75 @@ import java.util.Map.Entry;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class FindCommonCharacters {
+public class FindCommonCharacters{
+	
+	/*
+	 * 1002. Find Common Characters
+	 * https://leetcode.com/problems/find-common-characters/submissions/834768247/ 
+	 */
 
+	/*
+	 * Pseudocode:
+	 * Create a HashMap and insert characters and count of first word
+	 * Iterate through each word in the input list using for loop
+	 * Create a new HashMap to store the updated Map
+	 * check if each characters of first map is in the second word
+	 * add the matching character and its count in the updated Map
+	 * copy the update map to first Map and iterate until the end of word list
+	 * after all iterations, exit the loop and add each characters to list based on the value
+	 * 
+	 */
+	
 	@Test
 	public void commonChars1() {
-		
-		String[] words = {"bella","label","roller"};
-		Assert.assertEquals(Arrays.asList("e","l","l"), commonChars(words));
+
+		String[] words = { "bellla", "label", "roller" };
+		commonChars(words);
+		Assert.assertEquals(Arrays.asList("e", "l", "l"), commonChars(words));
 	}
-	
+
 	@Test
 	public void commonChars2() {
-		
-		String[] words = {"cool","lock","cook"};
-		commonChars(words);
+
+		String[] words = { "cool", "lock", "cook" };
+		Assert.assertEquals(Arrays.asList("c", "o"), commonChars(words));
 	}
-	
-	   public List<String> commonChars(String[] words) {  
-		   char[] firstWord = words[0].toCharArray();
-		   List<String> matchList = new ArrayList<>();
-		   int min =Integer.MAX_VALUE;
-		   HashMap<String,Integer> ncount =new HashMap<String, Integer>();
-		   int counter =0;
-		   for (char c : firstWord) {
-			   for(int i=1;i<words.length;i++) {
-				   if(words[i].indexOf(c)!=-1) {
-					   for(int j=0;j<words[i].length();j++) {
-						   if(words[i].charAt(j)== c)
-							   counter+=1;
-					   }
-					   ncount.put(String.valueOf(c), ncount.getOrDefault(String.valueOf(c), Math.min(min, counter)));
-					   counter=0;
-				   }else {
-					   ncount.remove(String.valueOf(c));
-				   }
-			   }
-	    }
-		for(Entry<String, Integer> entry : ncount.entrySet()) {
-			for(int i=0;i<entry.getValue();i++) 
-				matchList.add(entry.getKey());           
+
+	@Test
+	public void commonChars3() {
+
+		String[] words = { "acabcddd", "bcbdbcbd", "baddbadb", "cbdddcac", "aacbcccd", "ccccddda", "cababaab",
+				"addcaccd" };
+		Assert.assertEquals(Arrays.asList(), commonChars(words));
+	}
+
+	public List<String> commonChars(String[] words) {
+
+		List<String> matchList = new ArrayList<>();
+		HashMap<Character, Integer> ncount = new HashMap<Character, Integer>();
+		for (int i = 0; i < words[0].length(); i++) {
+			ncount.put(words[0].charAt(i), ncount.getOrDefault(words[0].charAt(i), 0) + 1);
 		}
-		System.out.println(matchList);
+
+		for (int i = 1; i < words.length; i++) {
+			HashMap<Character, Integer> updcount = new HashMap<Character, Integer>();
+			for (int j = 0; j < words[i].length(); j++) {
+				if (ncount.containsKey(words[i].charAt(j))) {
+					updcount.put(words[i].charAt(j),
+							// because previous word may contain min character("lock","cook")
+							Math.min(ncount.get(words[i].charAt(j)), updcount.getOrDefault(words[i].charAt(j), 0) + 1));
+				}
+
+			}
+			ncount = updcount;
+		}
+
+		for (Entry<Character, Integer> entry : ncount.entrySet()) {
+			for (int i = 0; i < entry.getValue(); i++)
+				matchList.add(String.valueOf(entry.getKey()));
+		}
+
 		return matchList;
-}
+
+	}
 }
