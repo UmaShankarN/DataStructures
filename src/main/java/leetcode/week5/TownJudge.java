004ac054d97c02a7953d4d8b7cdea140cb4e5c80
UmaShankarN <numashankar.psg@gmail.com>
2022-11-09 03:58:33 +0000

    Nov 8 townjudge

--- src/main/java/leetcode/week5/TownJudge.java
+++ src/main/java/leetcode/week5/TownJudge.java
@@ -78,27 +78,22 @@ public class TownJudge {
 		Assert.assertEquals(-1, findJudge(n, trust));
 	}
 
-	
 	@Test
 	public void findJudge9() {
 
 		int n = 4;
-		int[][] trust = { { 1, 2 }, { 1, 3 }, { 2, 1}, { 2, 3}, { 1,4} , { 4,3}, { 4, 1}};
+		int[][] trust = { { 1, 2 }, { 1, 3 }, { 2, 1 }, { 2, 3 }, { 1, 4 }, { 4, 3 }, { 4, 1 } };
 		Assert.assertEquals(3, findJudge(n, trust));
 	}
-	
-	
+
 	/*
-	 * Check if length of given array is 1 then return the [0][1]
-	 * if number of people is 1 and the return n
-	 * for other cases, create two lists
-	 * add the not trusted in first list that is 0th index of all array
-	 * add the trusted in second list that is 1st index of all array
-	 * create a map and add the trusted identifier and its occurrence in the map, 
-	 * get the last picked number to a variable
-	 * check the size of map is 1 and count of picked number is greater than 1
-	 * return the number if the condition matches
-	 * else return -1
+	 * Check if length of given array is 1 then return the [0][1] if number of
+	 * people is 1 and the return n for other cases, create two lists add the not
+	 * trusted in first list that is 0th index of all array add the trusted in
+	 * second list that is 1st index of all array create a map and add the trusted
+	 * identifier and its occurrence in the map, get the last picked number to a
+	 * variable check the size of map is 1 and count of picked number is greater
+	 * than 1 return the number if the condition matches else return -1
 	 * 
 	 */
 	public int findJudge(int n, int[][] trust) {
@@ -118,22 +113,51 @@ public class TownJudge {
 		}
 
 		int picked = 0;
-		HashMap<Integer,Integer> trustcountmap = new HashMap<>();
+		HashMap<Integer, Integer> trustcountmap = new HashMap<>();
 		for (Integer integer : trusted) {
 
 			if (!nottrusted.contains(integer)) {
-				trustcountmap.put(integer, trustcountmap.getOrDefault(integer,0)+1);
-					picked = integer;}
+				trustcountmap.put(integer, trustcountmap.getOrDefault(integer, 0) + 1);
+				picked = integer;
+			}
 		}
 //		 System.out.println(nottrusted);
 //		 System.out.println(trusted);
 //		System.out.println(picked);
 //		System.out.println(trustcountmap);
-		if(trustcountmap.size()==1 && trustcountmap.get(picked)>1) 
-				 return picked;
-		
+		if (trustcountmap.size() == 1 && trustcountmap.get(picked) > 1)
+			return picked;
+
 		return -1;
 
+	}
+	
+	/*
+	 * declare an integer array with size n+1 because the array starts from 0 to n
+	 * for each 1D array in input trust 2D array,
+	 * decrement the index value of declared array by getting the position from the 0 the index of input array
+	 * as no one trusts
+	 * increment the index value of declared array by getting the position from the 1 the index of input array
+	 * as someone trusts
+	 * Iterate from 1 to n and return the value index of value that is equal to n-1
+	 * 
+	 */
+	public int findJudge2(int n, int[][] trust) {
+
+		int[] tru2 = new int[n + 1];
+		for (int[] in : trust) {
+			tru2[in[0]]--;
+			tru2[in[1]]++;
+		}
+
+		//System.out.println(Arrays.toString(tru2));
+
+		for (int i = 1; i <= n; i++) {
+			if (tru2[i] == n - 1)
+				return i;
+		}
+
+		return -1;
 	}
 
 }
