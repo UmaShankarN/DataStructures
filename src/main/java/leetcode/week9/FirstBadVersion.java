5f9c8ef9cb2ca6ed8429ad6462dfae73e3757b0b
UmaShankarN <numashankar.psg@gmail.com>
2022-12-16 06:57:51 +0000

    badversion

---
+++ src/main/java/leetcode/week9/FirstBadVersion.java
@@ -0,0 +1,65 @@
+package leetcode.week9;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class FirstBadVersion {
+
+	int bad =0;
+	
+	@Test
+	public void badVersion1() {
+		bad=4;
+		Assert.assertEquals(4, firstBadVersion(5));
+	}
+	
+	@Test
+	public void badVersion2() {
+		bad=1;
+		Assert.assertEquals(1, firstBadVersion(1));
+	}
+	
+	@Test
+	public void badVersion3() {
+		bad=1702766719;
+		Assert.assertEquals(1702766719, firstBadVersion(2126753390));
+	}
+	
+    public int firstBadVersion(int n) {
+
+        /* Pseudocode: Binary Search
+        * declare left=0, right =n-1 and middle=0
+        * check if middle version is false if it is, our version is higher, so move left next to middle
+        * if our middle version is true, check the previous version, if the previous version is false
+        * return the middle version which is the first bad version
+        * if it is not, move our right to left of middle, because there are some bad version towards the left
+        * By this our bad version will be confined to a good and bad version
+        */
+        int left=0, right=n-1, middle=0; 
+        
+        while(left<=right){
+        
+            middle=left+(right-left);
+            if(isBadVersion(middle) == false){
+                left = middle+1;
+            } else{
+                if(isBadVersion(middle-1) ==false)
+                    return middle;
+                else
+                    right = middle-1;
+            }
+            
+        }
+        return n;
+    }
+
+	private boolean isBadVersion(int middle) {
+		
+		if(middle == bad )
+			return true;
+		else
+			return false;
+	}
+    
+}
